78ebf7580c06c54f409ae88719da122fb54a0846
richard <ra22597@gmail.com>
2022-11-20 18:47:38 +0000

    initial commit of new classes

---
+++ src/main/java/com/researchspace/dataverse/entities/DatasetFile.java
@@ -0,0 +1,15 @@
+package com.researchspace.dataverse.entities;
+
+import java.util.List;
+
+public class DatasetFile {
+    private  String description;
+    private  String label;
+    private  String directoryLabel;
+    private  String datasetVersionId;
+    private List<String> categories;
+    private  boolean restricted;
+    private  int version;
+    private DatasetFileDetails dataFile;
+
+}
